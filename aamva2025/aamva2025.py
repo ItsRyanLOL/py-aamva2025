b5e42296da97d0611e79de3c45e673af0e92525d
Ryan <35852086+ItsRyanLOL@users.noreply.github.com>
2025-05-30 18:30:29 -0400

    Update aamva2025.py
    added ref to ver 10

--- aamva2025/aamva2025.py
+++ aamva2025/aamva2025.py
@@ -408,7 +408,7 @@ class AAMVA2025:
                 log("=== End Subfile ===")
                 read_offset += 10
 
-        elif version in (2, 3, 4, 5, 6, 7, 8, 9):
+        elif version in (2, 3, 4, 5, 6, 7, 8, 9, 10):
             # version 2 and later add a jurisdiction field
             jurisdiction_version = data[17:19]
             assert (
